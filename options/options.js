86cb3b9be452e1e31249ffd5cb2e524a7fb24f9c
Crowdbotics <team@crowdbotics.com>
2023-06-18 17:39:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "steep-mouse-41573",
+  url: "https://steep-mouse-41573.botics.co",
+  api: "https://steep-mouse-41573.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
